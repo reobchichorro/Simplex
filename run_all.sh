7e07b6a9d1549c72a07714f107e5ae4c0bb796a2
Rodrigo <reobcnegf@gmail.com>
2025-04-23 20:50:49 -0300

    Adjusting batchfile

--- run_all.sh
+++ run_all.sh
@@ -2,9 +2,10 @@
 
 # Create output directory if it doesn't exist
 mkdir -p res
+mkdir -p res2
 
 # Loop through all files in the 'files' directory
 for file in files/*; do
     filename=$(basename "$file")           # Extract filename without path
-    ./solve "$file" > "res/${filename}.txt"  # Run command and redirect output
+    ./solve "$file" > "res/${filename}.txt" 2> "res2/${filename}.txt" # Run command and redirect output
 done
